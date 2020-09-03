410a0c2a7b30d269fb3483b7cf8562ce498828e3
Cristian Parra <cristian.a.parra@gmail.com>
2020-09-03 15:40:40 -0400

    mostrando resultados API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Header from './component/Header'
 import Formulario from './component/Formulario'
+import Clima from './component/Clima'
 
 function App() {
 
@@ -8,12 +9,26 @@ function App() {
     ciudad: '',
     pais: ''
   });
-  const[consultar, guardarConsultar] = useState(false)
+  const [consultar, guardarConsultar] = useState(false)
+  const [resultado, guardarResultado] = useState({})
 
   const { ciudad, pais } = busqueda;
 
   useEffect(() => {
-    console.log(ciudad)
+    const consultarAPI = async () => {
+
+      if (consultar) {
+
+        const appId = '0168c8e646061d3efc41aac0d32bcfe8';
+        const url = ` http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+
+        guardarResultado(resultado)
+        guardarConsultar(false)
+      }
+    }
+    consultarAPI()
 
   }, [consultar])
 
@@ -34,7 +49,8 @@ function App() {
               />
             </div>
             <div className='col m6 s12'>
-              2
+              <Clima
+              resultado={resultado} />
             </div>
           </div>
 
